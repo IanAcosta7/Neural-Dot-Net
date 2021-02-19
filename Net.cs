fa44b9d467aa6a19a92eaa56f11ebfb862a5a1a3
IanAcosta7 <ianacosta700@gmail.com>
2021-02-18 23:50:46 -0300

    Agregar archivos de proyecto.

---
+++ Net.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralDotNet
+{
+    public class Net
+    {
+        #region Properties
+        private double[] inputs;
+
+        public double[] Inputs
+        {
+            get { return inputs; }
+            set { inputs = value; }
+        }
+
+        private double[] outputs;
+
+        public double[] Outputs
+        {
+            get { return outputs; }
+        }
+        #endregion
+        #region Methods
+        public Net()
+        {
+            Console.WriteLine("NET");
+        }
+        #endregion
+    }
+}
---
+++ Neural-Dot-Net-Tester/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NeuralDotNet;
+
+namespace NeuralDotNetTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Net net = new Net();
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
