34ffdc71c16b9fb4c1c867f526c0b801f904f783
IanAcosta7 <ianacosta700@gmail.com>
2021-02-20 21:17:27 -0300

    Renamed MultiLayerPerceptron namespace.

--- Neural-Dot-Net-Tester/Program.cs
+++ Neural-Dot-Net-Tester/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using NeuralDotNet;
+using NeuralDotNet.NeuralNetwork;
 
 namespace NeuralDotNetTester
 {
